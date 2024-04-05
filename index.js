534bf0cf9856d93dd78b227dbd0ff2475d7cc947
Michael Czechowski <m.czechowski@pons.de>
2024-04-05 12:00:40 +0200

    add graceful shutdown

--- index.js
+++ index.js
@@ -13,6 +13,22 @@ app.get('/api/data', async (req, res) => {
     });
 });
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
+});
+
+process.on("SIGINT", () => {
+    console.log("Received SIGINT. Closing server...");
+    server.close(() => {
+        console.log("Server closed.");
+        process.exit(0);
+    });
+});
+
+process.on("SIGTERM", () => {
+    console.log("Received SIGINT. Closing server...");
+    server.close(() => {
+        console.log("Server closed.");
+        process.exit(0);
+    });
 });
